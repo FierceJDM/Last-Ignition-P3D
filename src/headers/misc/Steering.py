7319ff27a9c79aeaff04b8fa0cb2d20264c600b9
Aissa <azergoun02@gmail.com>
2023-02-24 06:02:15 -0500

    Enhanced Steering

--- src/headers/misc/Steering.py
+++ src/headers/misc/Steering.py
@@ -1,8 +1,12 @@
-#  Definitive values for realistic steering 
+#  Definitive values for realistic steering (11/02/2023)
 
 class Steer():
     def Left(self):
-        if self.Vehicle.getCurrentSpeedKmHour() < 135:
+        if self.Vehicle.getCurrentSpeedKmHour() < 35:
+            self.Steering += self.dt * (15 - (13*self.Vehicle.getCurrentSpeedKmHour()/35))
+            self.Steering = min(self.Steering, (22 - (14*self.Vehicle.getCurrentSpeedKmHour()/35)))
+            self.SteerLimit = (-23 - (-9*self.Vehicle.getCurrentSpeedKmHour()/35))
+        elif self.Vehicle.getCurrentSpeedKmHour() < 135:
             self.Steering += self.dt * (15 - (13*self.Vehicle.getCurrentSpeedKmHour()/135))
             self.Steering = min(self.Steering, (8 - (7.8*self.Vehicle.getCurrentSpeedKmHour()/135)))
             self.SteerLimit = (-17 - (-14*self.Vehicle.getCurrentSpeedKmHour()/135))
@@ -16,7 +20,11 @@ class Steer():
 
 
     def Right(self):
-        if self.Vehicle.getCurrentSpeedKmHour() < 135:
+        if self.Vehicle.getCurrentSpeedKmHour() < 35:
+            self.Steering += self.dt * (-15 - (-13*self.Vehicle.getCurrentSpeedKmHour()/35))
+            self.Steering = max(self.Steering, (-22 - (-14*self.Vehicle.getCurrentSpeedKmHour()/35)))
+            self.SteerLimit = (23 - (9*self.Vehicle.getCurrentSpeedKmHour()/35))
+        elif self.Vehicle.getCurrentSpeedKmHour() < 135:
             self.Steering += self.dt * (-15 - (-13*self.Vehicle.getCurrentSpeedKmHour()/135))
             self.Steering = max(self.Steering, (-8 - (-7.8*self.Vehicle.getCurrentSpeedKmHour()/135)))
             self.SteerLimit = (17 - (14*self.Vehicle.getCurrentSpeedKmHour()/135))
